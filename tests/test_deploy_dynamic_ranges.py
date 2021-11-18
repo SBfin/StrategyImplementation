4ecbaf05f50ab920d816e8b84724fb31ba1f49c7
Christian Kobril <christiankobril@gmail.com>
2021-11-18 15:32:38 +0100

    test setbase and setlimit

--- tests/test_deploy_dynamic_ranges.py
+++ tests/test_deploy_dynamic_ranges.py
@@ -13,7 +13,7 @@ def test_constructor(DynamicRangesStrategy, vault, gov, keeper):
 
 
 def test_constructor_checks(DynamicRangesStrategy, vault, gov, keeper):
-    with reverts("threshold % tickSpacin"):
+    with reverts("threshold % tickSpacing"):
         gov.deploy(DynamicRangesStrategy, vault, 2401, 1200, 500, 600, keeper)
 
     with reverts("threshold % tickSpacing"):
@@ -37,6 +37,52 @@ def test_constructor_checks(DynamicRangesStrategy, vault, gov, keeper):
     with reverts("twapDuration"):
         gov.deploy(DynamicRangesStrategy, vault, 2400, 1200, 500, 0, keeper)
 
-# def test_set_base_check(DynamicRangesStrategy, vault, gov, keeper):
-# def test_set_limit_check(DynamicRangesStrategy, vault, gov, keeper):
-# trigger modifier
+
+def test_set_base_threshold(DynamicRangesStrategy, vault, gov, keeper):
+    strategy = gov.deploy(DynamicRangesStrategy, vault, 2400, 1200, 500, 600, keeper)
+    baseThreshold = strategy.baseThreshold()
+
+    strategy.setBaseThreshold(baseThreshold + 1200, { "from": keeper })
+
+    assert(baseThreshold + 1200 == strategy.baseThreshold())
+
+    newStrategy = gov.deploy(DynamicRangesStrategy, vault, 2400, 1200, 500, 600, keeper)
+    noKeeperAddress = '0xaaB5a17c0d9d09632F013d8b5E2353A77710dDc1'
+
+    with reverts("keeper"):
+        newStrategy.setBaseThreshold(baseThreshold + 1200, { "from": noKeeperAddress })
+
+    with reverts("threshold % tickSpacing"):
+        strategy.setBaseThreshold(baseThreshold + 1201, { "from": keeper })
+
+    with reverts("threshold > 0"):
+        strategy.setBaseThreshold(-1, { "from": keeper })
+
+    with reverts("threshold too high"):
+        strategy.setBaseThreshold(887280, { "from": keeper })
+
+
+
+
+def test_set_limit_threshold(DynamicRangesStrategy, vault, gov, keeper):
+    strategy = gov.deploy(DynamicRangesStrategy, vault, 2400, 1200, 500, 600, keeper)
+    limitThreshold = strategy.limitThreshold()
+
+    strategy.setLimitThreshold(limitThreshold + 1200, { "from": keeper })
+
+    assert(limitThreshold + 1200 == strategy.limitThreshold())
+
+    newStrategy = gov.deploy(DynamicRangesStrategy, vault, 2400, 1200, 500, 600, keeper)
+    noKeeperAddress = '0xaaB5a17c0d9d09632F013d8b5E2353A77710dDc1'
+
+    with reverts("keeper"):
+        newStrategy.setLimitThreshold(limitThreshold + 1200, { "from": noKeeperAddress })
+
+    with reverts("threshold % tickSpacing"):
+        strategy.setLimitThreshold(limitThreshold + 1201, { "from": keeper })
+
+    with reverts("threshold > 0"):
+        strategy.setLimitThreshold(-1, { "from": keeper })
+
+    with reverts("threshold too high"):
+        strategy.setLimitThreshold(887280, { "from": keeper })
