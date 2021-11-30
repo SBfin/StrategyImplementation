16d0c1278f777d1bb5d8f1eaa805a2fd4c1a9e58
AlbyGiaco <alby.giaco@live.it>
2021-11-30 14:56:20 +0100

    fix action payload

--- frontend/src/components/eth/TokenBalance.js
+++ frontend/src/components/eth/TokenBalance.js
@@ -72,12 +72,14 @@ export const tokenSlice = createSlice({
         state.allowanceDai = action.payload;
       },
       ratioToken: (state, action) => {
-        if(!action && !action.payload){
+        if(action && action.payload){
           if(Number(action.payload[0]) !== 0 || Number(action.payload[1]) !== 0) {
             state.ratioToken = decimalFormat(action.payload[0], state.decimalsEth) / decimalFormat(action.payload[1], state.decimalsDai)
           } else {
             state.ratioToken = 1
           }
+        } else {
+          state.ratioToken = 1
         }
         console.log('ratio : ' + state.ratioToken)
       }
