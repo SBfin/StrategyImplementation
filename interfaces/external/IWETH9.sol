a1c456535449a4726f84a15b32485411df34795c
Silvio Busonero <31106245+SBfin@users.noreply.github.com>
2021-12-10 11:34:51 +0100

    Revert "Withdraw (#15)"
    This reverts commit 6913cad6ea22949ef53a283559cb4e0d8a0b3974.

--- interfaces/external/IWETH9.sol
+++ interfaces/external/IWETH9.sol
@@ -6,5 +6,4 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IWETH9 is IERC20 {
     /// @notice Deposit ether to get wrapped ether
     function deposit() external payable;
-    function withdraw(uint wad) external;
 }
