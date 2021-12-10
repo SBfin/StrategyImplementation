6913cad6ea22949ef53a283559cb4e0d8a0b3974
Silvio Busonero <31106245+SBfin@users.noreply.github.com>
2021-12-10 11:34:31 +0100

    Withdraw (#15)
    * remove deposit eth
    
    * remove _balances
    
    * testing
    
    * simplify signature and adapt tests
    
    * refactoring
    
    * if statement
    
    * withdraw
    
    Co-authored-by: silvio__b <silvio.busonero@bridgedefi.finance>

--- tests/test_deposit_eth.py
+++ tests/test_deposit_eth.py
@@ -2,7 +2,9 @@ from brownie import chain, reverts, ZERO_ADDRESS
 import pytest
 from pytest import approx
 import random
-from collections import OrderedDict
+
+"""
+
 
 @pytest.mark.parametrize(
     "amount0Desired,amount1Desired",
@@ -277,6 +279,85 @@ def test_deposit_checks(vault, user, tokens, gov):
 
     with reverts("to"):
         vault.depositEth(1e8, 0, 0, vault, {"from": user, "value" : 1e8})
+
+"""
+
+
+def test_withdraw(
+    vaultAfterPriceMove,
+    strategy,
+    pool,
+    tokens,
+    getPositions,
+    gov,
+    user,
+    recipient,
+    keeper,
+):
+    vault = vaultAfterPriceMove
+
+    # Deposit and rebalance
+    vault.setAddressWeth(tokens[1], {"from" : gov})
+    gov.transfer(tokens[1], "50 ether")
+    tx = vault.deposit(1e8, 1e10, 0, 0, user, {"from": user})
+    shares, _, _ = tx.return_value
+    strategy.rebalance({"from": keeper})
+
+    # Store balances, supply and positions
+    balance0 = tokens[0].balanceOf(recipient)
+    balance1 = tokens[1].balanceOf(recipient)
+    totalSupply = vault.totalSupply()
+    total0, total1 = vault.getTotalAmounts()
+    basePos, limitPos = getPositions(vault)
+
+    # Withdraw all shares
+    tx = vault.withdrawEth(shares, 0, 0, recipient, {"from": user})
+    amount0, amount1 = tx.return_value
+
+    # Check is empty now
+    assert vault.balanceOf(user) == 0
+
+    # Check received right amount of tokens
+    assert tokens[0].balanceOf(recipient) - balance0 == amount0 > 0
+    assert tokens[1].balanceOf(recipient) - balance1 == amount1 > 0
+
+    # Check total amounts are in proportion
+    ratio = (totalSupply - shares) / totalSupply
+    total0After, total1After = vault.getTotalAmounts()
+    assert approx(total0After / total0) == ratio
+    assert approx(total1After / total1) == ratio
+
+    # Check liquidity in pool decreases proportionally
+    basePosAfter, limitPosAfter = getPositions(vault)
+    assert approx(basePosAfter[0] / basePos[0]) == ratio
+    assert approx(limitPosAfter[0] / limitPos[0]) == ratio
+
+    # Check event
+    assert tx.events["Withdraw"] == {
+        "sender": user,
+        "to": recipient,
+        "shares": shares,
+        "amount0": amount0,
+        "amount1": amount1,
+    }
+
+
+def test_withdraw_checks(vault, user, recipient):
+    tx = vault.deposit(1e8, 1e10, 0, 0, user, {"from": user})
+    shares, _, _ = tx.return_value
+
+    with reverts("shares"):
+        vault.withdraw(0, 0, 0, recipient, {"from": user})
+    with reverts("to"):
+        vault.withdraw(shares - 1000, 0, 0, ZERO_ADDRESS, {"from": user})
+    with reverts("to"):
+        vault.withdraw(shares - 1000, 0, 0, vault, {"from": user})
+
+    with reverts("amount0Min"):
+        vault.withdraw(shares - 1000, 1e18, 0, recipient, {"from": user})
+    with reverts("amount1Min"):
+        vault.withdraw(shares - 1000, 0, 1e18, recipient, {"from": user})
+
 
 
 
