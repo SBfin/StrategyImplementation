f5f924a348705edc5766e710b28254cb9da0a1f3
Alberto <alby.giaco@live.it>
2021-12-14 13:55:23 +0100

    commit connector

--- frontend/src/helpers/connector.js
+++ frontend/src/helpers/connector.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 export const MetaMask = new InjectedConnector({ supportedNetworks: [1, 3, 4, 5, 42, 421611] });
 
 const contracts = {
-  1337: { vault: "0x2c15A315610Bfa5248E4CbCbd693320e9D8E03Cc" }, //mainnet-fork
+  1337: { vault: "0x94f810D5F23F08e772A8d92Dd20f7561d4D6E28b" }, //mainnet-fork
   3: { vault: "0x6a209bee7225f04b78e8addcd69dda7a7efe5ec0" }, // Ropsten
   69: { vault: "0xa919F8Dd481dE4050F660738c0052a17d62c1d09" }, // Optimism Kovan
   421611: { vault: "0xD27435518D79c4aA605DEF9A5B369bcC709C9a9F" }, // Arbitrum
