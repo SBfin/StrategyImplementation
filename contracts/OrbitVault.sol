2fafeefcc6c50d166f397b7c4346eb1d66b5125f
ihsan younes <ihsan.younes@bridgedefi.finance>
2022-01-10 09:22:32 +0100

    wip depositEth

--- contracts/OrbitVault.sol
+++ contracts/OrbitVault.sol
@@ -58,66 +58,6 @@ contract OrbitVault is
         uint256 amount
     );
 
-    function depositEth(
-        uint256 amountTokenDesired,
-        uint256 amountTokenMin,
-        uint256 amountEthMin,
-        address to
-    )
-        external
-        nonReentrant
-        payable
-        returns (
-            uint256 shares,
-            uint256 amount0,
-            uint256 amount1
-        )
-    {      
-        require(amountTokenDesired > 0 || msg.value > 0, "amountTokenDesired or value");
-        require(to != address(0) && to != address(this), "to");
-
-        uint256 amount0Desired;
-        uint256 amount1Desired;
-        uint256 amount1Min;
-        uint256 amount0Min;
-
-        if (token0IsWeth) {
-            amount0Desired = msg.value;
-            amount1Desired = amountTokenDesired;
-            amount0Min = amountEthMin;
-            amount1Min = amountTokenMin;
-        } else {
-            amount0Desired = amountTokenDesired;
-            amount1Desired = msg.value;
-            amount0Min = amountTokenMin;
-            amount1Min = amountEthMin;
-        }
-        
-        // Poke positions so vault's current holdings are up-to-date
-        _poke(baseLower, baseUpper);
-        _poke(limitLower, limitUpper);
-
-        // Calculate amounts proportional to vault's holdings
-        (shares, amount0, amount1) = _calcSharesAndAmounts(amount0Desired, amount1Desired);
-        require(shares > 0, "shares");
-        require(amount0 >= amount0Min, "amount0Min");
-        require(amount1 >= amount1Min, "amount1Min");
-
-        // Pull in tokens from sender
-        IWETH9(weth).deposit{value: (token0IsWeth ? amount0 : amount1) }();
-        if (msg.value >  (token0IsWeth ? amount0 : amount1) ) safeTransferETH(msg.sender, msg.value - (token0IsWeth ? amount0 : amount1));
-        if (token0IsWeth) {
-            token1.safeTransferFrom(msg.sender, address(this), amount1);
-        }
-        else { 
-            token0.safeTransferFrom(msg.sender, address(this), amount0);
-        }
-
-        // Mint shares to recipient
-        _mint(to, shares);
-        emit Deposit(msg.sender, to, shares, amount0, amount1);
-        require(totalSupply() <= maxTotalSupply, "maxTotalSupply");
-    }
 
     function withdrawEth(
         uint256 shares,
@@ -165,16 +105,6 @@ contract OrbitVault is
         }
 
         emit Withdraw(msg.sender, to, shares, amount0, amount1);
-    }
-
-    /// @notice Transfers ETH to the recipient address
-    /// @dev Fails with `STE`
-    /// @param to The destination of the transfer
-    /// @param value The value to be transferred
-    function safeTransferETH(address to, uint256 value) internal {
-        (bool success, ) = to.call{value: value}("");
-        require(success, 'STE');
-        emit EthRefund(to, value);
     }
 
     fallback() external payable {
