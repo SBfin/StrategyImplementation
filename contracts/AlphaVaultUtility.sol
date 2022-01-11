7733164bdc15079d86b6b0f4c5a38c54c5e790a5
ihsan younes <ihsan.younes@bridgedefi.finance>
2022-01-11 14:55:32 +0100

    AlphaVaultUtility, refactor depositEth and withdrawEth

--- contracts/AlphaVaultUtility.sol
+++ contracts/AlphaVaultUtility.sol
@@ -17,32 +17,22 @@ import "../interfaces/IVault.sol";
 import "../interfaces/IOrbitVault.sol";
 import "./AlphaVault.sol";
 
-contract OrbitVault is 
-    IVault,
-    IUniswapV3MintCallback,
-    IUniswapV3SwapCallback,
-    ERC20,
-    ReentrancyGuard,
-    AlphaVault
-{   
+contract AlphaVaultUtility {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
     address immutable weth;
+    AlphaVault public immutable alphaVault;
     bool token0IsWeth;
 
     constructor(address _pool,
-        uint256 _protocolFee,
-        uint256 _maxTotalSupply,
-        address wethAddress) 
-
-        AlphaVault(_pool,
-        _protocolFee,
-        _maxTotalSupply) 
+        address _alphaVault,
+        address wethAddress)
         
         {
             weth = wethAddress;
             token0IsWeth = IUniswapV3Pool(_pool).token0() == wethAddress;
+            alphaVault = AlphaVault(_alphaVault);
         }
 
     event EthRefund(
@@ -84,31 +74,12 @@ contract OrbitVault is
             amount0Min = amountTokenMin;
             amount1Min = amountEthMin;
         }
-        
-        // Poke positions so vault's current holdings are up-to-date
-        _poke(baseLower, baseUpper);
-        _poke(limitLower, limitUpper);
-
-        // Calculate amounts proportional to vault's holdings
-        (shares, amount0, amount1) = _calcSharesAndAmounts(amount0Desired, amount1Desired);
-        require(shares > 0, "shares");
-        require(amount0 >= amount0Min, "amount0Min");
-        require(amount1 >= amount1Min, "amount1Min");
 
         // Pull in tokens from sender
         IWETH9(weth).deposit{value: (token0IsWeth ? amount0 : amount1) }();
         if (msg.value >  (token0IsWeth ? amount0 : amount1) ) safeTransferETH(msg.sender, msg.value - (token0IsWeth ? amount0 : amount1));
-        if (token0IsWeth) {
-            token1.safeTransferFrom(msg.sender, address(this), amount1);
-        }
-        else { 
-            token0.safeTransferFrom(msg.sender, address(this), amount0);
-        }
 
-        // Mint shares to recipient
-        _mint(to, shares);
-        emit Deposit(msg.sender, to, shares, amount0, amount1);
-        require(totalSupply() <= maxTotalSupply, "maxTotalSupply");
+        (shares, amount0, amount1) = alphaVault.deposit(amount0Desired, amount1Desired, amount0Min, amount1Min, to);
     }
 
     function withdrawEth(
@@ -119,26 +90,8 @@ contract OrbitVault is
     ) external nonReentrant returns (uint256 amount0, uint256 amount1) {
         require(shares > 0, "shares");
         require(to != address(0) && to != address(this), "to");
-        uint256 totalSupply = totalSupply();
 
-        // Burn shares
-        _burn(msg.sender, shares);
-
-        // Calculate token amounts proportional to unused balances
-        uint256 unusedAmount0 = getBalance0().mul(shares).div(totalSupply);
-        uint256 unusedAmount1 = getBalance1().mul(shares).div(totalSupply);
-
-        // Withdraw proportion of liquidity from Uniswap pool
-        (uint256 baseAmount0, uint256 baseAmount1) =
-            _burnLiquidityShare(baseLower, baseUpper, shares, totalSupply);
-        (uint256 limitAmount0, uint256 limitAmount1) =
-            _burnLiquidityShare(limitLower, limitUpper, shares, totalSupply);
-
-        // Sum up total amounts owed to recipient
-        amount0 = unusedAmount0.add(baseAmount0).add(limitAmount0);
-        amount1 = unusedAmount1.add(baseAmount1).add(limitAmount1);
-        require(amount0 >= amount0Min, "amount0Min");
-        require(amount1 >= amount1Min, "amount1Min");
+        (amount0, amount1) = alphaVault.withdraw(shares, amount0Min, amount1Min, to);
 
         // Push tokens to recipient
         if (token0IsWeth) {
@@ -155,8 +108,6 @@ contract OrbitVault is
                 safeTransferETH(to, amount1);
             }
         }
-
-        emit Withdraw(msg.sender, to, shares, amount0, amount1);
     }
 
     /// @notice Transfers ETH to the recipient address
